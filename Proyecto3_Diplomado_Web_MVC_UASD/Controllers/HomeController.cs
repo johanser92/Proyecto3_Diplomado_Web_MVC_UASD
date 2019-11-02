b28fd489e73e2a73ad0235a00b34ef113e342dcf
johanser92 <40702293+johanser92@users.noreply.github.com>
2019-11-01 21:10:35 -0400

    Agregar

---
+++ Proyecto3_Diplomado_Web_MVC_UASD/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto3_Diplomado_Web_MVC_UASD.Models;
+
+namespace Proyecto3_Diplomado_Web_MVC_UASD.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var personas = new List<Persona>
+            {
+                new Persona {Cedula="0001", Nombre="Johanser", Sexo="Masculino", Direccion="Tamayo"},
+                new Persona {Cedula="2023", Nombre="Mercedes", Sexo="Femenina", Direccion="Barahona"},
+                new Persona {Cedula="0123", Nombre="Juan", Sexo="Masculino", Direccion="Fondo Negro"},
+                new Persona {Cedula="1312", Nombre="Carla", Sexo="Femenina", Direccion="Bani"},
+            };
+            return View(personas);
+        }
+    }
+}
---
+++ Proyecto3_Diplomado_Web_MVC_UASD/Models/Persona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto3_Diplomado_Web_MVC_UASD.Models
+{
+    public class Persona
+    {
+        public string Cedula { get; set; }
+        public string Nombre { get; set; }
+        public string Sexo { get; set; }
+        public string Direccion { get; set; }
+    }
+}
